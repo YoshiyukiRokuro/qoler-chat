220b72aae1045a057a599295f7350d1afa50c58f
yoshiyuki.rokuro <yoshiyuki.rokuro@qoler.jp>
2025-06-27 14:24:06 +0900

    アプリ名の変更

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,23 @@
+// vue.config.js
+
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  // pagesオプションを追加します
+  pages: {
+    index: {
+      // エントリーポイント
+      entry: 'src/main.js',
+      // 使用するテンプレート
+      template: 'public/index.html',
+      // 出力されるファイル名
+      filename: 'index.html',
+      // ここでタイトルを指定することもできます
+      title: 'クオラチャット',
+      icon: 'public/icons/favicon.ico' // アイコンのパスを指定
+    }
+  },
   pluginOptions: {
     electronBuilder: {
       nodeIntegrationInWorker: true,
